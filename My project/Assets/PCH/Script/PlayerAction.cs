0ec0335764962e806f070ee605af78961f1a7c70
park chang heon <51786976+parkchangheon@users.noreply.github.com>
2022-09-23 18:00:52 +0900

    0923 unity push bu pch

--- My project/Assets/PCH/Script/PlayerAction.cs
+++ My project/Assets/PCH/Script/PlayerAction.cs
@@ -6,7 +6,8 @@ public class PlayerAction : MonoBehaviour
 {
     float h;
     float v;
-
+    [SerializeField] float Speed;
+    bool isHorizonMove;
     Rigidbody2D rigid;
 
     void Awake()
@@ -19,10 +20,20 @@ public class PlayerAction : MonoBehaviour
         h = Input.GetAxisRaw("Horizontal");
         v = Input.GetAxisRaw("Vertical");
 
+        bool hDown = Input.GetButtonDown("Horizontal"); //수평방향 눌렀을 때 
+        bool vDown = Input.GetButtonDown("Vertical");   //수직방향 눌렀을 때
+        bool hUp = Input.GetButtonUp("Horizontal");     //수평방향 떼었을 때 이제는 수평 방향 
+        bool vUp = Input.GetButtonUp("Vertical");       //수직방향 떼었을 때
+
+        if (hDown || vUp)  
+            isHorizonMove = true;
+        else if (vDown || hUp)
+            isHorizonMove = false;
     }
 
     void FixedUpdate()
     {
-        rigid.velocity = new Vector2(h, v);
+        Vector2 moveVec = isHorizonMove ? new Vector2(h, 0) : new Vector2(0, v);
+        rigid.velocity = moveVec * Speed;
     }
 }
