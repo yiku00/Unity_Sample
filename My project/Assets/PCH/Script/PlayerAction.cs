bccc36ef5a0d78b331f1f825ad27a4e9b5af21c5
park chang heon <51786976+parkchangheon@users.noreply.github.com>
2022-09-23 18:26:13 +0900

    animation_pch

--- My project/Assets/PCH/Script/PlayerAction.cs
+++ My project/Assets/PCH/Script/PlayerAction.cs
@@ -6,13 +6,15 @@ public class PlayerAction : MonoBehaviour
 {
     float h;
     float v;
-    [SerializeField] float Speed;
+    [SerializeField] public float Speed;
     bool isHorizonMove;
     Rigidbody2D rigid;
+    Animator anim;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
@@ -25,10 +27,31 @@ public class PlayerAction : MonoBehaviour
         bool hUp = Input.GetButtonUp("Horizontal");     //수평방향 떼었을 때 이제는 수평 방향 
         bool vUp = Input.GetButtonUp("Vertical");       //수직방향 떼었을 때
 
-        if (hDown || vUp)  
+        //눌렀다 뗏다 할때 위 좌 키 누르고 렉걸리는 것 방지
+        if (hDown)
             isHorizonMove = true;
-        else if (vDown || hUp)
+        else if (vDown)
             isHorizonMove = false;
+
+        else if (hUp || vUp)
+            isHorizonMove = h != 0;
+
+        // 애니메이션 진입 후 계속 콜하는 것 방지. 한번만 애니메이션 쏴줘서 이동 smooth 하게 처리
+        if (anim.GetInteger("hAxisRaw") != h)
+        {
+            anim.SetBool("isChange", true);
+            anim.SetInteger("hAxisRaw", (int)h);
+        }
+
+        else if (anim.GetInteger("vAxisRaw") != v)
+        {
+            anim.SetBool("isChange", true);
+            anim.SetInteger("vAxisRaw", (int)v);
+        }
+
+        else
+            anim.SetBool("isChange", false);
+
     }
 
     void FixedUpdate()
